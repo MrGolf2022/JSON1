c45992b287e07a52725e8e5f9da40b3ea7e0c76c
MrGolf2022 <jirawat.ch@tnk.ac.th>
2022-11-10 13:30:36 +0700

    Update TestJS.js

--- Week4/TestJS.js
+++ Week4/TestJS.js
@@ -1,1 +1,12 @@
 console.log(5);
+var myName = 'Jirawat';
+let lastName = 'Chuaykhun';
+
+const year = 2022;
+//year = 2023;
+
+let grade = 3.56;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`My name is ${myName}`);
